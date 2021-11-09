bc35701c33323fb9ec0da28ec78b24c229fd3ad5
NataliaShchelkunova <>
2021-11-09 12:34:05 +0300

    create basis of project

--- src/App.js
+++ src/App.js
@@ -2,4 +2,52 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:7000/allTasks").then((res) => {
+      setTasks(res.data.data);
+    });
+  });
+
+  const addNewTask = () => {
+    axios
+      .post("http://localhost:7000/createTask", {
+        text,
+        isCheck: true,
+      })
+      .then((res) => {
+        setText("");
+        setTasks(res.data.data);
+      });
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>ToDo List</h1>
+        <div className="one-line-input-and-button">
+          <input
+            className="add-task-input"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={() => addNewTask()}>Add new</button>
+        </div>
+      </header>
+      <div className="all-tasks-container">
+        {tasks.map((task, index) => (
+          <div key={`task-${index}`}>
+            <input type="checkbox" isCheck={task.isCheck} />
+            <span> {task.text} </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default App;
